a4a71069b7c0471ceedfec5b29d0c7b80b9246c3
Austin Evans <evansau1@msu.edu>
2019-03-31 15:29:51 -0400

    Requirements

--- shotdispenser.py
+++ shotdispenser.py
@@ -1,4 +1,4 @@
-from magstripe import MagStripe
+from magstripe import MagStripe, MagStripeError
 from lcddriver import LCD
 import RPi.GPIO as GPIO
 import time
@@ -26,16 +26,16 @@ def keyboard_input():
 
 def pump(on=True):
     if on:
-        GPIO.output(RELAY_PIN, GPIO.HIGH)
+        GPIO.output(RELAY_PIN, GPIO.LOW)
         logging.info('Relay on')
     else:
-        GPIO.output(RELAY_PIN, GPIO.LOW)
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
         logging.info('Relay off')
 
 def process_id_card(data, mag):
     try:
-        parsed_data = m.parse(data)
-        logging.info('Parsed data: ' + parsed_data)
+        parsed_data = mag.parse(data)
+        logging.info('Parsed data: ' + parsed_data['name'])
         return parsed_data
     except MagStripeError as e:
         logging.error(e)
@@ -44,11 +44,13 @@ def process_id_card(data, mag):
 if __name__ == "__main__":
     setup_logging()
     setup_gpio()
+    pump(on=False)
     lcd = LCD()
     mag = MagStripe()
     while True:
-        mag_data = keyboard_input()
         lcd.lcd_clear()
+        lcd.lcd_display_string('Scan your MSU ID')
+        mag_data = keyboard_input()
         logging.info('Keyboard read:', mag_data)
         result = process_id_card(mag_data, mag)
         if result is None:
@@ -56,5 +58,5 @@ if __name__ == "__main__":
         lcd.lcd_display_string(result['name'])
         lcd.lcd_display_string(result['pid'], line=2)
         pump(on=True)
-        time.sleep(10)
+        time.sleep(30)
         pump(on=False)
