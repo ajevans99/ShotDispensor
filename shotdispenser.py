5ac147e93ca509970e4a78d05ee93c728445a02b
Austin Evans <evansau1@msu.edu>
2019-03-31 16:17:02 -0400

    More speaking and phrases

--- shotdispenser.py
+++ shotdispenser.py
@@ -6,6 +6,11 @@ import os
 import sys
 import logging
 from gtts import gTTS
+import random
+
+PHRASES = ['Hurray!', 'Cheers!', 'Yay!', 'Go Green!', 'Yippee!', 'Shit yeah!', 'Oof!', 'Phew! Finally...',
+           'Boo-yah!', 'Ta-da!', 'Yippee!', 'Yo-ho-ho!', 'Ooh-la-la!', 'Mmmmm!', 'Yum!', 'Oops!',
+           'Bada bing bada boom!', 'Bon appétit!', 'Gosh dangit!', 'Hallelujah!', 'Psst!', 'Whoa!', 'Yahoo!']
 
 RELAY_PIN  = 11 # BOARD
 
@@ -48,6 +53,11 @@ def speak(text):
     speech.save("speech.mp3")
     os.system("mpg321 speech.mp3")
 
+def speak_random(name):
+    first_name = name.split()[0]
+    message = random.choice(PHRASES) + ' ' + first_name + ', your shot is ready!'
+    speak(message)
+
 if __name__ == "__main__":
     setup_logging()
     setup_gpio()
@@ -65,8 +75,8 @@ if __name__ == "__main__":
             continue
         lcd.lcd_display_string(result['name'])
         lcd.lcd_display_string(result['pid'], line=2)
-        speak(result['name'] + ' your shot is on the way!')
+        speak('Your shot is on the way!')
         pump(on=True)
         time.sleep(30)
         pump(on=False)
-        speak(result['name'] + ' your shot is ready!')
+        speak_random([result['name'])
